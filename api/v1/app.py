572ec5af288296965e1c71027494065888d79964
Tijani1402 <ridwanomofolarin14@gmail.com>
2022-10-24 16:51:31 -0700

    updates

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,72 +1,48 @@
 #!/usr/bin/python3
 """
-Flask App that integrates with AirBnB static HTML Template
+entry point of our application
 """
-from api.v1.views import app_views
-from flask import Flask, jsonify, make_response, render_template, url_for
-from flask_cors import CORS, cross_origin
-from flasgger import Swagger
+
+from os import getenv
+from flask import Flask, jsonify
 from models import storage
-import os
-from werkzeug.exceptions import HTTPException
+from api.v1.views import app_views
+from flask_cors import CORS
 
-# Global Flask Application Variable: app
+
 app = Flask(__name__)
-swagger = Swagger(app)
-
-# global strict slashes
-app.url_map.strict_slashes = False
-
-# flask server environmental setup
-host = os.getenv('HBNB_API_HOST', '0.0.0.0')
-port = os.getenv('HBNB_API_PORT', 5000)
-
-# Cross-Origin Resource Sharing
-cors = CORS(app, resources={r'/*': {'origins': host}})
-
-# app_views BluePrint defined in api.v1.views
 app.register_blueprint(app_views)
 
+cors = CORS(app, resources={r"/api/v1/*": {"origins": "0.0.0.0"}})
 
-# begin flask page rendering
+
 @app.teardown_appcontext
-def teardown_db(exception):
+def teardown(exception):
     """
-    after each request, this method calls .close() (i.e. .remove()) on
-    the current SQLAlchemy Session
+    a method that closes the storage
     """
     storage.close()
 
 
-@app.errorhandler(Exception)
-def global_error_handler(err):
+@app.errorhandler(404)
+def error_404(error):
     """
-        Global Route to handle All Error Status Codes
+    handles the error
     """
-    if isinstance(err, HTTPException):
-        if type(err).__name__ == 'NotFound':
-            err.description = "Not found"
-        message = {'error': err.description}
-        code = err.code
+    return jsonify({
+        'error': 'Not found'
+    }), 404
+
+
+if __name__ == '__main__':
+    if getenv('HBNB_API_HOST'):
+        host = getenv('HBNB_API_HOST')
     else:
-        message = {'error': err}
-        code = 500
-    return make_response(jsonify(message), code)
+        host = "0.0.0.0"
 
+    if getenv('HBNB_API_PORT'):
+        port = getenv('HBNB_API_PORT')
+    else:
+        port = 5000
 
-def setup_global_errors():
-    """
-    This updates HTTPException Class with custom error function
-    """
-    for cls in HTTPException.__subclasses__():
-        app.register_error_handler(cls, global_error_handler)
-
-
-if __name__ == "__main__":
-    """
-    MAIN Flask App
-    """
-    # initializes global error handling
-    setup_global_errors()
-    # start Flask app
-    app.run(host=host, port=port)
+    app.run(host=host, port=port, threaded=True)
